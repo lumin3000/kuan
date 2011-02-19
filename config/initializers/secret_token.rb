a0afa45c2dabdffa8d34e886f036766e2da26195
Whyme.Lyu <5longluna@gmail.com>
2011-02-19 16:53:13 +0800

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kuan::Application.config.secret_token = '4215821c78b0041aac55c276b3df9fb9e6eb8db26a68b64dee525e7f45fabef9c4a5331fda5b4ced584d3bd6042bc4f9f678baa07f809a5514bd19fe061dd5f4'
