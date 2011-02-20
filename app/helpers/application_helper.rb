b13d960854bc94ea7f862d1ee4f710dc54036a4d
Whyme.Lyu <5longluna@gmail.com>
2011-02-20 13:49:18 +0800

    Views should be plural

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   def render_post(p)
     type = p.class.to_s.downcase!
-    template = "post/#{type}"
-    render partial: "post/post", object: p,
+    template = "posts/#{type}"
+    render partial: "posts/post", object: p,
       locals: { sub_template: template }
   end
 end
