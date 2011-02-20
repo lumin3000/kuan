1658873ba99e6ff6f1d460c345baf3a648deef08
Whyme.Lyu <5longluna@gmail.com>
2011-02-20 18:17:49 +0800

    Basic Image model

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,27 @@
+require 'mini_magick'
+require 'pp'
+
 class Image
   include Mongoid::Document
 
+  field :description
+  field :small
+  field :large
+  field :original
+
+  def self.create_from_original(file, desc = nil)
+    image = Image.new()
+    image.description = desc
+
+    grid = Mongo::Grid.new(image.db)
+    original_path = "image/#{image._id.to_s}/o.jpg"
+
+    id = grid.put(File.open(file),
+                  filename: original_path,
+                  content_type: "image/jpg",
+                  )
+    image.original = id
+    image.save
+    image
+  end
 end
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
-describe Image do
-  #pending "add some examples to (or delete) #{__FILE__}"
+describe Image, "create_from_original" do
+  describe "given an image file" do
+    filename = Rails.root.join("test", "fixtures", "mxgs239.jpg")
+    subject { Image.create_from_original(filename) }
+    it "should provide thumbnails" do
+      subject.should be_kind_of(Image)
+      subject._id.should_not be_nil
+      subject.original.should_not be_nil
+    end
+  end
 end
