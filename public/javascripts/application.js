0fecac5aaf039c7ad6b9d1bab46ce3ecddb777fd
fancyoung <siyang1982@msn.com>
2011-02-21 21:00:23 +0800

    ajax file upload

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,29 +1,59 @@
-function remote_file(id, path, cb){
+var K = {};
+
+K.remote_file = function(id, path, cb){
     var file = $(id);
-    var f_id = '_fff';
-    var fr = new Element('iframe', {'id': f_id}).
+    if(!file){
+        throw new Error('Can not find file element');
+    }
+    if(!path){
+        throw new Error('Path is empty');
+    }
+    function frame_load(){
+        var that = this;
+        function on_success(){
+            file_feed.set('html', '继续上传');
+            var v = that.contentDocument.body.innerHTML;
+            v = JSON.decode(v);
+            cb && cb.bind(that)(v);
+        }
+        function on_error(){
+        }
+        on_success();
+        
+        file.set('disabled', false);
+        file_submit.set('disabled', false);
+    }
+    var file_clone = file.clone();
+    var file_tmp = 'file_upload';
+    var f_tar = '_fff_'+Number.random(1,9999);
+    var fr = new Element('iframe', {'id': f_tar, 'name': f_tar}).
         inject(document.body, 'after').
         setStyles({
-            'height': 60,
-            'width': 360
+            'position': 'absolute',
+            'top': '-1000px',
+            'left': '-1000px'
         }).addEvent('load', frame_load);
-    function frame_load(){
-        cb && cb(this.contentDocument.body.innerHTML);
-    }
-    new Element('input', {
+    var file_feed = new Element('span', {
+        'id': file_tmp,
+        'html': '上传新文件'
+    }).inject(file, 'after');
+    var file_submit = new Element('input', {
         'type': 'submit', 'value': '上传'
-    }).inject(file, 'after').addEvent('click', function(){
+    }).inject(file_feed, 'after').addEvent('click', function(){
+        if(this.get('disabled') == true || file.value == ''){
+            return false;
+        }
+        this.set('disabled', true);
         var f = new Element('form', {
             'action': '/images',
             'enctype': 'multipart/form-data',
             'method': 'post',
-            'target': f_id
-        }).inject(document.body);
-        new Element('span', {
-            'id': 'aaa',
-            'html': '开始上传'
-        }).inject(file, 'after');
+            'target': f_tar
+        }).inject(document.body).setStyle('display', 'none');
         file.inject(f);        
+        file_feed.set('html', '开始上传');
+        file = file_clone.inject(file_feed, 'before').set('disabled', true);
+        file_clone = file_clone.clone();
         f.submit();
     });
 }
