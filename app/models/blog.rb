12c4f5d5ab808ae270eb8892882d2f95c0bd36e3
Whyme.Lyu <5longluna@gmail.com>
2011-02-21 17:13:39 +0800

    Validation for Blog

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,12 @@
+# encoding: utf-8
+
 class Blog < Post
   field :title
   field :content
-  
+
   attr_accessible :title, :content
+
+  validates_length_of :content,
+    :minimum => 1,
+    :too_short => "写点什么吧",
 end
