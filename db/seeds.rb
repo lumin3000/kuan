0a60581e0cfc5d535feed3186c47e21b18b6e815
Whyme.Lyu <5longluna@gmail.com>
2011-02-21 16:05:23 +0800

    Remove all tweet stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,6 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
-Tweet.create content: "foooo"
-Tweet.create content: "很长很长的中文tweet一枚很长很长的中文tweet一很长很长的中文tweet一枚枚很长很长的中文tweet一枚"
-
 Link.create url: "http://g.cn", title: "Google", content: "This is it!"
 
 Blog.create({
