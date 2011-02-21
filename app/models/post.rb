86575e6f0533d497d4b84ab6068255e4542306e9
fancyoung <siyang1982@msn.com>
2011-02-21 11:08:37 +0800

    form destroy

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post
   include Mongoid::Document
   include Mongoid::Timestamps
   
+  def haml_object_ref
+    "post"
+  end
+
   def self.infer_type t
     klass = Object.const_get t.capitalize
     if self.subclasses.include? klass
