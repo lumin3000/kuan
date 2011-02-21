af99e7d5b0caf9e0f33ba5f41e2bb36200fd4753
Whyme.Lyu <5longluna@gmail.com>
2011-02-21 14:56:49 +0800

    Remove desc field from Image

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,18 +3,16 @@ require 'mini_magick'
 class Image
   include Mongoid::Document
 
-  field :description
   field :small
   field :large
   field :original
 
   Versions = [:original]
 
-  def self.create_from_original(file, desc = nil)
+  def self.create_from_original(file)
     raise "File not available" if not file.respond_to? :read
 
     image = Image.new()
-    image.description = desc
 
     grid = Mongo::Grid.new(image.db)
     original_path = "image/#{image._id.to_s}/o.jpg"
