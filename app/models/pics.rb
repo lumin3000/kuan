614f34ea28e9ec7693806e29625ff4b19cce0cdb
Whyme.Lyu <5longluna@gmail.com>
2011-02-22 12:23:03 +0800

    Fix update issue for embedded doc

--- app/models/pics.rb
+++ app/models/pics.rb
@@ -4,5 +4,16 @@ class Pics < Post
 
   attr_accessible :photos, :content
 
+  def update_attributes(attrs = {})
+    photos = attrs.delete :photos
+    if not photos.nil?
+      photos = photos.map do |p|
+        Photo.new(p)
+      end
+    end
+    attrs[:photos] = photos
+    super(attrs)
+  end
+
   validates_length_of :photos, :minimum => 1
 end
--- spec/models/pics_spec.rb
+++ spec/models/pics_spec.rb
@@ -1,17 +1,21 @@
 describe Pics do
+  before :all do
+    @images = Image.limit(2).all.to_ary
+    raise "not enouuuuuuuuuuuuuuuuugh" if @images.length < 2
+    params = {
+      photos: [
+        {image: @images[0].id.to_s, desc: "Photo1"},
+        {image: @images[1].id.to_s, desc: "yet another photo"},
+      ],
+      content: "Blah"
+    }
+    @post = Pics.new(params)
+  end
+
   describe "Given a params hash from client" do
     it "should create a complete pics model" do
-      @images = Image.limit(2).all.to_ary
-      raise "not enouuuuuuuuuuuuuuuuugh" if @images.length < 2
-      @params = {
-        photos: [
-          {image: @images[0].id.to_s, desc: "Photo1"},
-          {image: @images[1].id.to_s, desc: "yet another photo"},
-        ],
-        content: "Blah"
-      }
-      @post = Pics.create(@params)
       @post.should be_valid
+      @post.save.should be_true
 
       image = @post.photos[0].image
       url = image.url_for(:original)
@@ -20,6 +24,19 @@ describe Pics do
 
       posts = Post.all.to_ary
       posts.should be_include(@post)
+    end
+  end
+
+  describe "Given a param for existing pics" do
+    it "should be able to update" do
+      @post.update_attributes!({
+        photos: [{image: @images[1].id.to_s, desc: ""}],
+        content: "",
+      })
+      @post.reload
+      @post.photos.length.should == 1
+      @post.photos[0].desc.should be_empty
+      @post.content.should be_empty
     end
   end
 end
