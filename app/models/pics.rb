a40484f7405c4e483341ba5ed825db560f0eede3
Whyme.Lyu <5longluna@gmail.com>
2011-02-22 15:10:00 +0800

    Handle photo upsert by ourselves

--- app/models/pics.rb
+++ app/models/pics.rb
@@ -7,13 +7,25 @@ class Pics < Post
   def update_attributes(attrs = {})
     photos = attrs.delete :photos
     if photos.is_a? Array
-      photos = photos.map do |p|
-        i = Image.criteria.id(p[:image]).first
-        p[:image] = i
-        Photo.new(p)
+      new_photos = []
+      photos.each do |p|
+        id = p.delete :id
+        if id.nil?
+          i = Image.criteria.id(p[:image]).first
+          next if i.nil?
+          p[:image] = i
+          np = Photo.new(p)
+          self.photos << np
+          np.save
+        else
+          photo = self.photos.detect do |p|
+            p._id.to_s == id
+          end
+          next if photo.nil?
+          photo.update_attributes!(p) if photo
+        end
       end
     end
-    attrs[:photos] = photos
     super(attrs)
   end
 
