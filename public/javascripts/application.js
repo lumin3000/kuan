05566927e8acb61bc0f28d1fe45947497566cdd9
fancyoung <siyang1982@msn.com>
2011-02-22 11:59:28 +0800

    for pics

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -34,9 +34,8 @@ K.remote_file = function(id, path, cb){
         on_success(v);
         
         file.set('disabled', false);
-        file_submit.set('disabled', false);
+        //file_submit.set('disabled', false);
     }
-    var file_clone = file.clone();
     var file_tmp = 'file_upload';
     var f_tar = '_fff_'+Number.random(1,9999);
     var fr = new Element('iframe', {'id': f_tar, 'name': f_tar}).
@@ -49,17 +48,27 @@ K.remote_file = function(id, path, cb){
     setTimeout(function(){
         fr.addEvent('load', frame_loaded);
     }, 50);
+    var file_btn = new Element('input', {
+        'type': 'button',
+        'value': '上传文件:)'
+    }).inject(file, 'before').setStyles({
+        'font-size':12,
+        'overflow':'hidden',
+        'position':'absolute'
+    });
+    var file_clone = file.clone();
     var file_feed = new Element('span', {
         'id': file_tmp,
         'html': '上传新文件'
     }).inject(file, 'after');
-    var file_submit = new Element('input', {
+    /*var file_submit = new Element('input', {
         'type': 'submit', 'value': '上传'
-    }).inject(file_feed, 'after').addEvent('click', function(){
+    }).inject(file_feed, 'after')*/
+    function file_event(){
         if(this.get('disabled') == true || file.value == ''){
             return false;
         }
-        this.set('disabled', true);
+        //this.set('disabled', true);
         var f = new Element('form', {
             'action': '/images',
             'accept-charset': 'UTF-8',
@@ -70,8 +79,9 @@ K.remote_file = function(id, path, cb){
         }).inject(document.body).setStyle('display', 'none');
         file.inject(f);        
         file_feed.set('html', '开始上传');
-        file = file_clone.inject(file_feed, 'before').set('disabled', true);
+        file = file_clone.inject(file_feed, 'before').set('disabled', true).addEvent('change', file_event);
         file_clone = file_clone.clone();
         f.submit();
-    });
+    }
+    file.addEvent('change', file_event);
 }
