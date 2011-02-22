eef118fccd9cf0a25d1a79e1ff95e97304848ad6
lilu <lilu.life@gmail.com>
2011-02-22 11:24:39 +0800

    add lib pinyin spec

--- spec/libs/pinyin_lib_spec.rb
+++ spec/libs/pinyin_lib_spec.rb
@@ -5,7 +5,8 @@ require 'pinyin'
 describe PinYin do
   describe "pinyin translation" do
     it "should give correct pinyin" do
-      PinYin.instance.to_pinyin("李路").should == "lilu" 
+      PinYin.instance.to_pinyin("李路").should == "lilu"
+      PinYin.instance.to_pinyin("leslie").should == "leslie" 
     end
   end
 end
