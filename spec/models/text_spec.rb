6ed0e2e240bcbdd43d51ee72cc5752fcde0336e2
Whyme.Lyu <5longluna@gmail.com>
2011-02-25 12:34:11 +0800

    More spec for Post, incomplete anyway

--- spec/models/text_spec.rb
+++ spec/models/text_spec.rb
@@ -3,15 +3,27 @@ require 'spec_helper'
 describe Text do
   before :all do
     @blog = Factory :blog, :uri => Factory.next(:uri)
-    @owage = Factory :following, {
+    owage = Factory :following, {
       auth: "founder",
       blog: @blog,
     }
-    @user = Factory :user, :followings => [@owage], :email => Factory.next(:email)
+    @founder = Factory :user, :followings => [owage], :email => Factory.next(:email)
+
+    attendance = Factory :following, {
+      auth: "member",
+      blog: @blog,
+    }
+    @member = Factory :user, :followings => [attendance], :email => Factory.next(:email)
+
+    primary_blog = Factory :following, {
+      auth: "lord",
+      blog: @blog,
+    }
+    @lord = Factory :user, :followings => [primary_blog], :email => Factory.next(:email)
 
     @params = {
       blog_id: @blog.id.to_s,
-      author_id: @user.id.to_s,
+      author_id: @founder.id.to_s,
     }
   end
 
@@ -54,9 +66,52 @@ describe Text do
     it "should not validate" do
       @text = Text.new({
         content: "whatever",
-        author_id: @user.id,
+        author_id: @founder.id,
       })
     end
   end
-  
+
+  describe "Given a post in attended blog" do
+    it "should not be editable by the user" do
+      @post = Text.create!(@params.dup.update({
+        content: "blah",
+      }))
+
+      @post.should_not be_editable_by(@member)
+    end
+
+    describe "stuff posted by member" do
+      before :all do
+        @post_by_member = Text.create!(@params.dup.update({
+          content: "my two cents",
+          author_id: @member.id.to_s,
+        }))
+      end
+
+      it "should be editable by founder" do
+        @post_by_member.should be_editable_by(@founder)
+      end
+
+      it "should be editable by lord" do
+        @post_by_member.should be_editable_by(@lord)
+      end
+    end
+  end
+
+  describe "Given a follower of the blog" do
+    it "should not be able to post stuff" do
+      following = Factory :following, {
+        :auth => "follower",
+        :blog => @blog,
+      }
+      @follower = Factory :user, :followings => [following],
+        :email => Factory.next(:email)
+      @post = Text.new({
+        content: "messing around",
+        author_id: @follower.id.to_s,
+        blog_id: @blog.id.to_s,
+      })
+      @post.should_not be_valid
+    end
+  end
 end
