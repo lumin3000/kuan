62b92941a84e4872c00d5b5357926730f5ca052d
fancyoung <siyang1982@msn.com>
2011-03-02 22:36:48 +0800

    add error message

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,7 +7,7 @@ class Comment
   referenced_in :author, :class_name => 'User'
   attr_accessible :content, :post, :author, :author_id
 
-  validates_presence_of :content, :message => "empty content"
+  validates_presence_of :content, :message => "回复不能为空"
   validates_length_of :content,
     :maximum => 1000,
     :too_long => "最多%{count}个字"
