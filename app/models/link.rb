63e701ea1891affbd00263094d96fa37b9504b04
Whyme.Lyu <5longluna@gmail.com>
2011-03-02 19:56:29 +0800

    Remove redundant validation

--- app/models/link.rb
+++ app/models/link.rb
@@ -12,17 +12,6 @@ class Link < Post
 
   validates_presence_of :url,
     :message => "请输入链接"
-  validates_length_of :url,
-    :minimum => 3,
-    :maximum => 100,
-    :too_short => "网址格式不正确",
-    :too_long => "网址格式不正确"
-
-  validates_presence_of :title,
-    :message => "请输入标题"
-  validates_length_of :title,
-    :maximum => 60,
-    :too_long => "标题太长了"
 
   private
 
