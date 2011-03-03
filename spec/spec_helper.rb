2a2086612472d088202bf1571180ac0aff06f164
Whyme.Lyu <5longluna@gmail.com>
2011-03-03 15:32:58 +0800

    Let spork handle database cleanup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,16 +14,13 @@ Spork.prefork do
 
   RSpec.configure do |config|
     config.mock_with :rspec
-
-    config.after :suite do
-      Mongoid.master.collections.select do |collection|
-        collection.name !~ /system/
-      end.each(&:drop)
-    end
-
   end
 end
 
 Spork.each_run do
   require 'factory_girl_rails'
+
+  Mongoid.master.collections.select do |collection|
+    collection.name !~ /system/
+  end.each(&:drop)
 end
