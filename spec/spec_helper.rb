4d723cbd727c753f16ee98ebd912b46f126e5790
Whyme.Lyu <5longluna@gmail.com>
2011-03-03 13:41:41 +0800

    Reload factory_girl on each run so it works

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,11 @@
 require 'rubygems'
 require 'spork'
 
+ENV["RAILS_ENV"] ||= 'test'
+
 Spork.prefork do
-  ENV["RAILS_ENV"] ||= 'test'
   require 'rails/mongoid'
-  Spork.trap_class_method(Rails::Mongoid, :load_models)
+  Spork.trap_class_method Rails::Mongoid, :load_models
 
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
@@ -24,4 +25,5 @@ Spork.prefork do
 end
 
 Spork.each_run do
+  require 'factory_girl_rails'
 end
