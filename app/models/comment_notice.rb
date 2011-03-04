364fde9b9987287e6942e543c7144f80750fa9d2
fancyoung <siyang1982@msn.com>
2011-03-04 15:24:31 +0800

    working on comments notices

--- app/models/comment_notice.rb
+++
@@ -1,9 +0,0 @@
-class CommentNotice
-  include Mongoid::Document
-  include Mongoid::Timestamps
-  
-  field :unread, :type => Boolean, :default => true
-  referenced_in :post
-
-  embedded_in :user, :inverse_of => :comment_notices
-end
