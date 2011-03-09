700cfb4c5e5bd86f9ece17dd7d14b131faae6b94
fancyoung <siyang1982@msn.com>
2011-03-09 18:07:09 +0800

    pagination_standard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -43,4 +43,29 @@ module ApplicationHelper
       :collection => collection,
     }.update(options)
   end
+  
+  def pagination_standard(options = {})
+    options = { max_page: 10, show_page: 10 }.update(options)
+
+    if m = %r{page/(\d+)/?$}.match(request.url)
+      current_page, base_url = m[1].to_i(10), m.pre_match
+    else
+      current_page, base_url = 1, request.url
+    end
+    current_page = current_page
+    base_url.sub! %r{/?$}, ""
+
+    if current_page < 4
+      start_page = 1
+    elsif current_page > options.max_page - options.show_page + 4
+      start_page = options.max_page - options.show_page + 1
+    else
+      start_page = current_page
+    end
+    render partial: "shared/pagination_standard", locals: {
+      current_page: current_page,
+      start_page: start_page,
+      base_url: base_url,
+    }.update(options)
+  end
 end
