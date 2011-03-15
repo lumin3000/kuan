f8cddb54046764af59757e2484d6844d915dc119
fancyoung <siyang1982@msn.com>
2011-03-15 15:16:08 +0800

    fix select bug in firefox && ie

--- public/javascripts/post_form.js
+++ public/javascripts/post_form.js
@@ -1,6 +1,7 @@
 K.widgets.changeTag = function(el){
   el.addEvent('change', function(e){
-    var v = this.options(this.selectedIndex).get('data-tags')
+    var item = $(this.options[this.selectedIndex])
+    var v = item.get('data-tags')
     var textbox = $$('.tags')[0].textboxlist
     if(!v) return
     if(textbox.original.value != textbox.default_value) return
