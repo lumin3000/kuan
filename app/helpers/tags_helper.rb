0aee0b6d604579689e20f0548ac7cedafdb108db
fancyoung <siyang1982@msn.com>
2011-03-15 11:29:24 +0800

    fix bug

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -4,7 +4,7 @@ module TagsHelper
   end
 
   def tags_for_post_form(post, blog)
-    post.tags << blog.tag if post.tags.blank? && !blog.nil? && !blog.tag.blank?
+    post.tags = [blog.tag] if post.tags.blank? && !blog.nil? && !blog.tag.blank?
     tags_value post.tags
   end
 
--- public/javascripts/post_form.js
+++ public/javascripts/post_form.js
@@ -1,9 +1,9 @@
-document.addEvent('domready', function(){
-  $('blog_id').addEvent('change', function(e){
+K.widgets.changeTag = function(el){
+  el.addEvent('change', function(e){
     var v = this.options(this.selectedIndex).get('data-tags')
     var textbox = $$('.tags')[0].textboxlist
     if(!v) return
     if(textbox.original.value != textbox.default_value) return
     $$('.tags')[0].textboxlist.reset(v)
   })
-})
+}
