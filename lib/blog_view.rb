e9a87f3a9081658e1187cb3efbd065f94f3cc538
Whyme.Lyu <5longluna@gmail.com>
2011-03-16 12:10:43 +0800

    Reverted double braces monkey patch

--- lib/blog_view.rb
+++ lib/blog_view.rb
@@ -283,16 +283,3 @@ TPL
 end
 
 Dir[Rails.root.join('lib/object_view/*.rb')].each {|f| require f}
-
-# MMMMMMMMOONKEY PAAAATCH!
-class Mustache
-  class Parser
-    def otag
-      @otag ||= '{'
-    end
-
-    def ctag
-      @ctag ||= '}'
-    end
-  end
-end
