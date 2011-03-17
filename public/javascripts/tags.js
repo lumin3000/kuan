a7e2115a2a2030397e73d5c3b84df893dc294b8d
fancyoung <siyang1982@msn.com>
2011-03-17 19:18:27 +0800

    tag trend js

--- public/javascripts/tags.js
+++ public/javascripts/tags.js
@@ -1,2 +1,12 @@
 document.addEvent('domready', function(){
+  $$('.activity .item').each(function(item){
+    console.log(item.get('title'))
+    var count = item.get('title').replace(/.*:\s/, '')
+    item.getElement('div').setStyle('height', trend_height(count))
+  })
 })
+
+function trend_height(n){
+  if(!n)n=0
+  return Math.min(n*2+1, 40)
+}
