c6156ae496e85644a0cb600c505a45c3a5bade3d
fancyoung <siyang1982@msn.com>
2011-03-17 20:52:48 +0800

    tag index

--- public/javascripts/tags.js
+++ public/javascripts/tags.js
@@ -1,8 +1,10 @@
 document.addEvent('domready', function(){
+  var i = 1
   $$('.activity .item').each(function(item){
     console.log(item.get('title'))
     var count = item.get('title').replace(/.*:\s/, '')
     item.getElement('div').setStyle('height', trend_height(count))
+    i++%2 == 1 && item.addClass('odd')
   })
 })
 
@@ -10,3 +12,12 @@ function trend_height(n){
   if(!n)n=0
   return Math.min(n*2+1, 40)
 }
+
+K.puzzle = (function(){
+  return {
+    init: function(){
+    },
+    move: function(){
+    }
+  }
+})()
