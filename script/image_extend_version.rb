31d40be96b1b6c61d26854b065dcb1984c45fb90
lilu <lilu.life@gmail.com>
2011-03-18 17:20:30 +0800

    image extend del blog

--- script/image_extend_version.rb
+++ script/image_extend_version.rb
@@ -8,30 +8,10 @@ MISSING_VERSION = {
     :'150' => [150, 150],
     :'100' => [100, 0],
     :'75' => [75, 75],
-  },
-  blog_icon: {
-    :'128' => [128, 128],
-    :'96' => [96, 96],
-    :'64' => [64, 64],
-    :'48' => [48, 48],
-    :'40' => [40, 40],
-    :'30' => [30, 30],
-    :'16' => [16, 16],
-  },
+  }
 }
 
 logger = Logger.new(Rails.root + 'log' + 'extend_image.log')
-
-Blog.all.each do |b|
-  i = b.icon
-  if i.nil? || i.id.nil?
-    logger.info "Blog #{b.uri} has no icon"
-    next
-  end
-  i.extend_version(MISSING_VERSION[:blog_icon])
-  i.save!
-  logger.info "Did blog #{b.uri}"
-end
 
 Pics.all.each do |post|
   post.photos.each do |photo|
