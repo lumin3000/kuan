c82e6ec40fe07ec81249971f19081fee9084ab35
lilu <lilu.life@gmail.com>
2011-03-27 18:07:06 +0800

    add controller business config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
   include UrlHelper
+
+  def business_config
+    YAML.load_file "#{Rails.root}/config/business.yml"
+  end
 end
