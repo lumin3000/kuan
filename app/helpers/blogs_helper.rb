e3aa25e6e46b12776e63540c509e9fac03a084a7
hhhhhh <hhhhhh@hhhhhh.(none)>
2011-04-21 12:42:07 +0800

    primary&public&primary logo

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,4 +2,14 @@ module BlogsHelper
   def follow_tag(blog)
     render partial: "blogs/follow_toggle", locals: { blog: blog }
   end
+
+  def blog_type(blog)
+    if blog.primary?
+      "primary"
+    elsif blog.private?
+      "private"
+    else
+      "public"
+    end
+  end
 end
