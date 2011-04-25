95ac7abb58978a72c65530c60d0d26641e54a323
Whyme.Lyu <5longluna@gmail.com>
2011-04-25 13:17:56 +0800

    Extract OAuthTarget class, pretty Java-ish

---
+++ app/models/oauth_target.rb
@@ -0,0 +1,91 @@
+class OAuthTarget < SyncTarget
+  require 'oauth'
+  require 'net/http'
+  require 'net/http/post/multipart'
+  require 'uri'
+
+  field :status, :type => Symbol, :default => :waiting_for_auth
+  field :token_key
+  field :token_secret
+  field :account
+
+  def self.auth(blog, controller)
+    target = where(:blog_id => blog.id,
+                          :token_key => controller.params[:oauth_token]).first
+    unless target
+      controller.render :status => 404
+      return
+    end
+
+    access_token = target.request_token.get_access_token(
+      :oauth_verifier => controller.params[:oauth_verifier])
+    unless access_token
+      controller.render :status => 400
+      return
+    end
+    after_auth(target, access_token)
+    target.token = access_token
+    target.status = :verified
+    if target.save
+      controller.render 'sync/setup_success', :layout => false
+    else
+      controller.render :status => 500
+    end
+  end
+
+  def self.consumer
+    @consumer ||= OAuth::Consumer.new consumer_key, consumer_secret,
+      :site => site
+  end
+
+  def self.apply(blog, controller)
+    blog.sync_targets.each do |t|
+      t.destroy if t.class == self
+    end
+
+    request_token = consumer.get_request_token
+    if create( :token_key => request_token.token,
+              :token_secret => request_token.secret,
+              :blog_id => blog.id)
+      callback_url = controller.blog_path(blog) + 'sync_callback/sina_weibo'
+      controller.redirect_to request_token.authorize_url(
+        :oauth_callback => callback_url)
+    else
+      controller.render :status => 500
+    end
+  end
+
+  def self.logger
+    @logger ||= Logger.new 'log/sync.log'
+  end
+
+  def token=(token)
+    self.token_key = token.token
+    self.token_secret = token.secret
+  end
+
+  def request_token
+    OAuth::RequestToken.new self.consumer, token_key, token_secret
+  end
+
+  def access_token
+    @access_token ||= OAuth::AccessToken.new self.consumer, token_key, token_secret
+  end
+
+  def consumer
+    self.class.consumer
+  end
+
+  def handle_post(post)
+    return unless self.status == :verified
+    return if post.class == Post
+    method = "handle_#{post.class.name.downcase}"
+    return unless self.respond_to? method
+    begin
+      response = self.send method, post
+      raise response.body if response.code == '400' || response.code == '401'
+    rescue Exception => e
+      self.class.logger.info e.to_s.force_encoding('utf-8')
+    end
+  end
+end
--- app/models/sina_weibo.rb
+++ app/models/sina_weibo.rb
@@ -1,106 +1,31 @@
 # encoding: utf-8
 
-class SinaWeibo < SyncTarget
-  # FIXME: extract oauth logic to a new super class
-  require 'oauth'
-  require 'net/http'
-  require 'net/http/post/multipart'
+class SinaWeibo < OAuthTarget
   require 'json'
-  require 'uri'
-
-  field :status, :type => Symbol, :default => :waiting_for_auth
-  field :token_key
-  field :token_secret
-  field :account
-
-  CONSUMER_KEY = '609051831'
-  CONSUMER_SECRET = 'debf621a6f856f9cd08b855522bdb2a1'
   SITE = 'http://api.t.sina.com.cn/'
 
-  def self.apply(blog, controller)
-    blog.sync_targets.each do |t|
-      t.destroy if t.class == self
+  class << self
+    def consumer_key
+      '609051831'
     end
 
-    request_token = consumer.get_request_token
-    if create( :token_key => request_token.token,
-              :token_secret => request_token.secret,
-              :blog_id => blog.id)
-      callback_url = controller.blog_path(blog) + 'sync_callback/sina_weibo'
-      controller.redirect_to request_token.authorize_url(
-        :oauth_callback => callback_url)
-    else
-      controller.render :status => 500
+    def consumer_secret
+      'debf621a6f856f9cd08b855522bdb2a1'
+    end
+
+    def after_auth(target, access_token)
+      response = access_token.get '/account/verify_credentials.json'
+      response = JSON.load response.body
+      target.account = response['name']
     end
   end
 
-  def self.auth(blog, controller)
-    target = where(:blog_id => blog.id,
-                          :token_key => controller.params[:oauth_token]).first
-    unless target
-      controller.render :status => 404
-      return
-    end
-
-    access_token = target.request_token.get_access_token(
-      :oauth_verifier => controller.params[:oauth_verifier])
-    unless access_token
-      controller.render :status => 400
-      return
-    end
-    response = access_token.get '/account/verify_credentials.json'
-    response = JSON.load response.body
-    target.account = response['name']
-    target.token = access_token
-    target.status = :verified
-    if target.save
-      controller.render 'sync/setup_success', :layout => false
-    else
-      controller.render :status => 500
-    end
-  end
-
-  def self.consumer
-    @consumer ||= OAuth::Consumer.new CONSUMER_KEY, CONSUMER_SECRET,
-      :site => SITE
+  def self.site
+    SITE
   end
 
   def self.grid
     @grid ||= Mongo::Grid.new self.db
-  end
-
-  def self.logger
-    @logger ||= Logger.new 'log/sync.log'
-  end
-
-  def token=(token)
-    self.token_key = token.token
-    self.token_secret = token.secret
-  end
-
-  def request_token
-    OAuth::RequestToken.new self.consumer, token_key, token_secret
-  end
-
-  def access_token
-    @access_token ||= OAuth::AccessToken.new self.consumer, token_key, token_secret
-  end
-
-  def consumer
-    self.class.consumer
-  end
-
-  def handle_post(post)
-    return unless self.status == :verified
-    return if post.class == Post
-    method = "handle_#{post.class.name.downcase}"
-    return unless self.respond_to? method
-    begin
-      response = self.send method, post
-      raise response.body if response.code == '400' || response.code == '401'
-    rescue Exception => e
-      self.class.logger.info e.to_s.force_encoding('utf-8')
-    end
   end
 
   def handle_text(post)
