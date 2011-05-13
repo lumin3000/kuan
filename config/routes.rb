3cbc7d1a0e1ddbd3a2e0e6ae009401178e427dab
fancyoung <siyang1982@msn.com>
2011-05-13 17:02:14 +0800

    add js css for categories

--- config/routes.rb
+++ config/routes.rb
@@ -93,7 +93,7 @@ Kuan::Application.routes.draw do
 
   get "/categories/manage", :to => "categories#manage", :as => "categories_manage"
   post "/categories/batch", :to => "categories#batch", :as => "categories_batch"
-  get "/categories/:name" => "categories#show", :as => 'category'
+  get "/categories/c/:name" => "categories#show", :as => 'category'
   resources :categories do
     resources :category_subs
   end
--- public/javascripts/category.js
+++ public/javascripts/category.js
@@ -1,2 +1,39 @@
 document.addEvent('domready', function(){
+  //
+  setInterval((function(){
+    var tops = $$('.spread_banner')
+    var length = tops.length
+    var index = 0
+    return function(){
+      tops.setStyle('z-index', 1)
+      tops[index].setStyle('z-index', 2)
+      ++index>=length && (index=0)
+      tops[index].setStyle('z-index', 3).show()
+      tops[index].getElement('img').fade('hide').fade('in')
+    }
+  })(), 5000)
+
+  //
+  var el = $$('.categories_box')[0]
+  var cat = new Fx.Scroll(el, {
+    duration: 1500,
+    transition:Fx.Transitions.Quad.easeInOut
+  })
+  $$('.left').addEvent('click', function(){
+    console.log('left')
+    cat.start(el.getScroll().x-600, 0)
+  })
+  $$('.right').addEvent('click', function(){
+    console.log('right')
+    cat.start(el.getScroll().x+600, 0)
+  })
+
+  //
+  var bubbles = $$('.category_list a')
+  var sea = new Element('div', {
+    'class': 'spread_tag'
+  }).inject($$('.category_list')[0], 'after')
+  for(var i=0,l=bubbles.length; i<l; i++){
+    bubbles[i].inject(sea).addClass('bubble'+(i+1))
+  }
 })
