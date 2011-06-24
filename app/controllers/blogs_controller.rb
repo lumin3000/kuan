aee6ccc41450ca76855292eb6a11acc75a3f5856
fancyoung <siyang1982@msn.com>
2011-06-24 16:44:21 +0800

    fix bug: 手机置回复为已读

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -88,6 +88,8 @@ PREVENT_CLICK
       end
       if params[:post_id]
         @post = Post.find(params[:post_id])
+        notice = current_user.comments_notices.get_by_post(@post).first
+        notice.read! unless notice.nil?
         render 'posts/show'
       else
         cur_page = params[:page].to_i
